2e2f1e475aaf84b117c589c482655b8194e8a2ac
greenknot <greenknot@users.noreply.github.com>
2019-05-23 14:49:34 +0200

    tests: add aes

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -7,6 +7,7 @@ function main()
     local dir
     dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
+    qemu-arm-static "${dir}/test_aes"
     qemu-arm-static "${dir}/test_hmac"
     qemu-arm-static "${dir}/test_ripemd"
     qemu-arm-static "${dir}/test_sha2"
